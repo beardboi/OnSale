081944e750da7fd49e2b5f753f7469e8f45221bf
beardboi <mr.k3rn3lp4n1c@gmail.com>
2021-01-26 03:50:39 -0300

    feat(Common): add Country entity

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        /*
+         * The id of the country.
+         * Unique
+         */ 
+        [Key]
+        public int Id { get; set; }
+
+        /*
+         * The name of the country
+         */
+        [MaxLength(50, ErrorMessage = "The field {0} must contain less than {1} characters.")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
