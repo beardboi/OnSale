e1978185e92b2c52dc2da9c05ae5b5e665d3aa58
beardboi <mr.k3rn3lp4n1c@gmail.com>
2021-01-28 20:54:05 -0300

    feat(web): add helpers

---
+++ OnSale.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,24 @@
+﻿using OnSale.Common.Entities;
+using OnSale.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Helpers
+{
+    public interface IConverterHelper
+    {
+        // To convert from Category to CategoryViewModel
+        Category ToCategory(CategoryViewModel model, Guid imageId, bool isNew);
+
+        // To convert from CategoryViewModel to Category
+        CategoryViewModel ToCategoryViewModel(Category category);
+
+        // To convert from ProductViewModel to Product
+        Task<Product> ToProductAsync(ProductViewModel model, bool isNew);
+
+        // To convert from Product to ProductViewModel
+        ProductViewModel ToProductViewModel(Product product);
+    }
+}
---
+++ OnSale.Web/Helpers/IUserHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnSale.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Helpers
+{
+    public interface IUserHelper
+    {
+        // Get the user by the email, return 
+        Task<User> GetUserAsync(string email);
+
+        // Add the user given a user and a password.
+        // Return 
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        // Check the role.
+        Task CheckRoleAsync(string roleName);
+
+        // Add role to a user.
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        // Check is a user has the role.
+        // Return boolean
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+    }
+
+}
